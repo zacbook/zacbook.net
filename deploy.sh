91ee77a790cad3cbc535a06da7752defbe82abc3
Zac Harmany <zac@zacbook.net>
2017-11-19 22:18:49 -0800

    Configure deploy script to use zacbook name and email.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ rscript -e "blogdown::build_site()"
 cd ./public
 git init .
 git add .
+git config user.name "Zac Harmany"
+git config user.email "zac@zacbook.net"
 git commit -m "Deploy"
 git push git@github-zacbook:zacbook/zacbook.net.git --force master
 rm -rf .git
