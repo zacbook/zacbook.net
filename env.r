0065ce7de8dff7003b52886bcd9fb6da82ebd5ae
Zachary T. Harmany <zharmany@gmail.com>
2021-01-29 12:38:48 -0800

    Added rmarkdown and knitr, not sure if needed.

--- env.r
+++ env.r
@@ -2,6 +2,10 @@
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
+# Install RMarkdown
+install.packages('rmarkdown')
+# Install Knitr
+install.packages('knitr')
 # Install Blogdown
 install.packages("blogdown")
 # Install Hugo
