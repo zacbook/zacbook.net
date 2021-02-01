c31d9ec272d514a666688749473ea6ed8ba9c661
Zac Harmany <zac@zacbook.net>
2021-02-01 14:30:11 -0800

    Added comment in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 rm -rf ./public
 mkdir ./public
+# To ALWAYS rebuild the .Rmd to .html files, use the build_rmd=TRUE flag
+# rscript -e "blogdown::build_site(build_rmd=TRUE)"
 rscript -e "blogdown::build_site()"
 cd ./public
 git init -b 'master' .
