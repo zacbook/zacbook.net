0906a6fc4abdc4e60f45d1215afe62bc91926251
Zachary T. Harmany <zharmany@gmail.com>
2021-02-01 23:22:30 -0800

    Added explicit case of Rscript in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 rm -rf ./public
 mkdir ./public
 # To ALWAYS rebuild the .Rmd to .html files, use the build_rmd=TRUE flag
-rscript -e "blogdown::build_site(build_rmd=TRUE)"
+Rscript -e "blogdown::build_site(build_rmd=TRUE)"
 cd ./public
 git init -b 'master' .
 git add .
